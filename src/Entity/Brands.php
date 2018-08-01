a7cbf3cea0f5b8f8f5b9dcf71c75537417e92a98
Dan <danschmit@protonmail.com>
2018-08-01 15:07:15 +0200

    req checker

---
+++ src/Entity/Brands.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\BrandsRepository")
+ */
+class Brands
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=256)
+     */
+    private $brand;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getBrand(): ?string
+    {
+        return $this->brand;
+    }
+
+    public function setBrand(string $brand): self
+    {
+        $this->brand = $brand;
+
+        return $this;
+    }
+}
