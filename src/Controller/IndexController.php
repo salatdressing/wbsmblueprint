6a7fa841e4c03ee32c39f2a06acabfac1ed02a6d
Dan <danschmit@protonmail.com>
2018-08-02 08:59:32 +0200

    added use statement for response

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use App\Entity\Brands;
+use Symfony\Component\HttpFoundation\Response;
 
 class IndexController extends Controller
 {
